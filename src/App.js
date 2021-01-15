e0c94ae91fb62185d75edc4f1cae6ea82cb2aefa
Aaron <a.rcottrill521@gmail.com>
2021-01-15 17:43:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+import React, { Component } from 'react';
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faTumblr } from '@fortawesome/free-brands-svg-icons'
+class App extends Component {
+
+
+  constructor() {
+    super();
+    this.state = {
+      quoteId: -1
+    }
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.fetchQuotes();
+  }
+
+  componentDidMount() {
+    this.fetchQuotes();
+  }
+
+  /**
+   * Uses a quote object to set object properties
+   * @param {String} json
+   */
+  updateQuote(json) {
+    var cleanName = json.author.replace(/ +/g, "%20");
+    var cleanContent = json.content.replace(/ +/g, '%20');
+
+    document.getElementById('author').textContent = json.author;
+    document.getElementById('text').textContent = json.content;
+    document.getElementById('tweet-quote').href = "https://www.twitter.com/intent/tweet?text=" + cleanName + ":%0A" + cleanContent;
+    document.getElementById('tumblr-quote').href = "http://tumblr.com/widgets/share/tool?posttype=quote" +
+      "&tags=quotes&caption=" + cleanName
+      + "&content=" + cleanContent + "&canonicalUrl="
+      + "quote-machine.com";
+  }
+
+  fetchQuotes() {
+    fetch('https://api.quotable.io/random')
+      // Handle success
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        return this.updateQuote(json);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+
+
+  render() {
+    return (
+
+      <div className="center topOfPage" id="quote-box">
+
+        <p id="text" className="twentyPoint italic">
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p id="author" className="twentyPoint">
+        </p>
+        <button id="new-quote" className="twentyPoint" onClick={this.onClick}>
+          Get a new quote
+        </button>
+        <br />
+        <br />
+        <a id="tweet-quote" title="Tweet this quote to your friends" href="./">
+          <FontAwesomeIcon icon={faTwitter} className="thirtyTwoPoint peachText" />
         </a>
-      </header>
-    </div>
-  );
+        <a id="tumblr-quote" title="Post this quote on Tumblr" href="./">
+
+          <FontAwesomeIcon icon={faTumblr} className="thirtyTwoPoint redText" />
+        </a>
+
+
+      </div >
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,160 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, fireEvent, render, queryByAttribute, waitFor } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container;
+
+
+beforeEach(() => {
+
+  var mockResponse = {
+    "_id": "8mHI-1eW7cg",
+    "tags": [
+      "famous-quotes"
+    ],
+    "content": "Change me.",
+    "author": "Dummy",
+    "length": 53
+  }
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockResponse)
+  });
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+  jest.restoreAllMocks();
+});
+test('expect to have a query box', () => {
+  const dom = render(<App />);
+  const getById = queryByAttribute.bind(null, 'id');
+  var queryBox = getById(dom.container, 'quote-box');
+  expect(queryBox).toBeInTheDocument();
+});
+test('expect to have a text element in the query box', () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+  const getById = queryByAttribute.bind(null, 'id');
+  var queryBox = getById(container, 'quote-box');
+  const textElement = getById(queryBox, 'text');
+  expect(textElement).toBeInTheDocument();
+});
+test('expect to have an author element in the query box', () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+  const getById = queryByAttribute.bind(null, 'id');
+  var queryBox = getById(container, 'quote-box');
+  const authorElement = getById(queryBox, 'author');
+  expect(authorElement).toBeInTheDocument();
+});
+test('expect to have an new-quote button in the query box', () => {
+  const dom = render(<App />);
+  const getById = queryByAttribute.bind(null, 'id');
+  var queryBox = getById(dom.container, 'quote-box');
+  const newQuoteButton = getById(queryBox, 'new-quote');
+  expect(newQuoteButton).toBeInTheDocument();
+});
+test('expect to have an tweet quote button in the query box', () => {
+  const dom = render(<App />);
+  const getById = queryByAttribute.bind(null, 'id');
+  var queryBox = getById(dom.container, 'quote-box');
+  const tweetQuoteButton = getById(queryBox, 'tweet-quote');
+  expect(tweetQuoteButton).toBeInTheDocument();
+});
+test('expect to have a quote on load', async () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+
+  });
+  const getById = queryByAttribute.bind(null, 'id');
+  var queryBox = getById(container, 'quote-box');
+  const textElement = getById(queryBox, 'text');
+  await waitFor(() => expect(textElement).toHaveTextContent("Change me"));
+});
+test('expect to have the quote author on load', async () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+  const getById = queryByAttribute.bind(null, 'id');
+  var queryBox = getById(container, 'quote-box');
+  const authorElement = getById(queryBox, 'author');
+  await waitFor(() => expect(authorElement).toHaveTextContent("Dummy"));
+});
+test('get a new quote on click', async () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+  jest.restoreAllMocks();
+
+  var mockResponse = {
+    "_id": "8mHI-1eW7cg",
+    "tags": [
+      "famous-quotes"
+    ],
+    "content": "Changed .",
+    "author": "Dummy2",
+    "length": 53
+  }
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockResponse)
+  });
+
+  const getById = queryByAttribute.bind(null, 'id');
+  var queryBox = getById(container, 'quote-box');
+  const quoteElement = getById(queryBox, 'text');
+  const newQuoteButton = getById(queryBox, 'new-quote');
+  fireEvent.click(newQuoteButton);
+  await waitFor(() => expect(quoteElement).toHaveTextContent("Changed"));
+});
+test('get a new quote author on click', async () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+  jest.restoreAllMocks();
+
+  var mockResponse = {
+    "_id": "8mHI-1eW7cg",
+    "tags": [
+      "famous-quotes"
+    ],
+    "content": "Changed .",
+    "author": "Dummy2",
+    "length": 53
+  }
+  jest.spyOn(global, 'fetch').mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockResponse)
+  });
+
+  const getById = queryByAttribute.bind(null, 'id');
+  var queryBox = getById(container, 'quote-box');
+  const authorElement = getById(queryBox, 'author');
+  const newQuoteButton = getById(queryBox, 'new-quote');
+  fireEvent.click(newQuoteButton);
+  await waitFor(() => expect(authorElement).toHaveTextContent("Dummy2"));
+});
+test('expect to be able to tweet quote', async () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+
+  const getById = queryByAttribute.bind(null, 'id');
+  var queryBox = getById(container, 'quote-box');
+  const tweetButton = getById(queryBox, 'tweet-quote');
+  await waitFor(() => expect(tweetButton).toHaveAttribute('href', "https://www.twitter.com/intent/tweet?text=Dummy:%0AChange%20me."));
+});
+test('expect quote-box to be centered', () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+
+  });
+  const getById = queryByAttribute.bind(null, 'id');
+  var quoteBox = getById(container, 'quote-box');
+  expect(quoteBox).toHaveClass("center");
 });
